7058fe386ed941174afa00a1b9f52988b4cf4675
hetpatel733 <hetpatel7627@gmail.com>
2026-03-03 20:11:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # if you are using bash, uncomment the line below
-/bin/bash ./bash/run-bash.sh
+# /bin/bash ./bash/run-bash.sh
 
 # if you are using javascript, uncomment the line below
-# /bin/bash ./javascript/run-javascript.sh
+/bin/bash ./javascript/run-javascript.sh
 
 # if you are using python, uncomment the line below
 # /bin/bash ./python/run-python.sh
